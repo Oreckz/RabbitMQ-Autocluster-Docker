f30222156c12942115ae015de62f6164446f3f3b
Patrick Birchall <pbirchall900@gmail.com>
2017-08-03 12:19:11 +0100

    adding script to upgrade to docker-ce

---
+++ scripts/update_to_docker-ce.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#Warn the user because we are nice
+echo "Running this will remove the current Docker installation"
+read -r -p "Are you sure? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        upgrade_docker()
+        ;;
+    *)
+        return 0
+        ;;
+esac
+
+function upgrade_docker {
+
+    #Gracefully shut down and remove Docker
+    docker stop $(docker ps -q)
+    sudo systemctl stop docker
+    sudo apt remove docker.io
+
+    #Add official Docker apt repo
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+
+    #Install docker-ce from official repo
+    sudo apt-get install -y docker-ce
+}
